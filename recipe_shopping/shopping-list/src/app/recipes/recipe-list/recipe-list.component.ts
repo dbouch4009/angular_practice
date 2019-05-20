e57ff3dd5d161b8401c2051027c984452c1ec211
Damian Bouch <dbouch4009@gmail.com>
2019-05-20 14:48:30 -0400

    first leg of course project done

--- recipe_shopping/shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe_shopping/shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [         //type assignment using TS
+    new Recipe('BBQ Chicken','Delicious and grilled','https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Chicken_BBQ.jpg/1200px-Chicken_BBQ.jpg'),
+    new Recipe('BBQ Chicken','Delicious and grilled','https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Chicken_BBQ.jpg/1200px-Chicken_BBQ.jpg')
+  ];  
 
   constructor() { }
 
