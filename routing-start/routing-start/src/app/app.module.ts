b3afe20d87b90036b91097b9c98f11473e129488
Damian Bouch <dbouch4009@gmail.com>
2019-05-30 10:09:36 -0400

    up to child routing works

--- routing-start/routing-start/src/app/app.module.ts
+++ routing-start/routing-start/src/app/app.module.ts
@@ -11,6 +11,37 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import {Routes, RouterModule} from '@angular/router';  //needed for routes
+
+const appRoutes: Routes = [
+  { // full path is localhost:4200/users
+    path: 'users',
+    component: UsersComponent, children: [
+      {
+        path: ':id/:name',  //with colon, allows for dynamic querying
+        component: UserComponent
+      }
+    ] //which component gets loaded when route is hit
+  },
+  {
+    path: '',  //home page
+    component: HomeComponent
+  },
+  {
+    path: 'servers',
+    component: ServersComponent,  //child routing
+    children:[
+      {
+        path: ':id',
+        component: ServerComponent
+      },
+      {
+        path: ':id/edit',
+        component: EditServerComponent
+      },
+    ]
+  }
+];
 
 @NgModule({
   declarations: [
@@ -25,7 +56,8 @@ import { ServersService } from './servers/servers.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)  //needed for routes, argument from above declaration
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
--- routing-start/routing-start/src/app/users/user/user.component.ts
+++ routing-start/routing-start/src/app/users/user/user.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -7,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
-  constructor() { }
+  constructor(private activeRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.user = {
+      id: this.activeRoute.snapshot.params['id'],  //fetching from route query, users/:id/:name
+      name: this.activeRoute.snapshot.params['name']
+    };
+    //this.paramsSubscription = this.activeRoute.params  //optional part if one's own observables are implemented
+      this.activeRoute.params
+      .subscribe(
+        //this will fire whenever the params change via clicking Bogart's button,
+        // cont'd or anything else that changes the URL route
+        (params: Params) => {
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        }             
+      );
   }
+
+  //optional, but if you add your own observables this is necessary
+  // ngOnDestroy(){
+  //   this.paramsSubscription.unsubscribe();
+  // }
 
 }
